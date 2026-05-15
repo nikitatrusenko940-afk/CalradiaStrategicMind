3b1a55dcfbe27ffac65e313379b11f0e8b44803b
nikitatrusenko940-afk <nikitatrusenko940@gmail.com>
2026-05-16 01:52:23 +0200

    Enable short defense diagnostics logs

--- Source/CalradiaStrategicMind/Settings/DryRunDefenseSettings.cs
+++ Source/CalradiaStrategicMind/Settings/DryRunDefenseSettings.cs
@@ -3,8 +3,8 @@ namespace CalradiaStrategicMind.Settings
     public static class DryRunDefenseSettings
     {
         public static bool EnableDefenseDiagnostics = true;
-        public static bool EnableVerboseDefenseLogs = true;
-        public static bool EnableDefenseCandidateLogs = true;
+        public static bool EnableVerboseDefenseLogs = false;
+        public static bool EnableDefenseCandidateLogs = false;
         public static bool EnableDefenseSummaryLogs = true;
         public static bool EnableDryRunDefenseController = true;
         public static bool EnableDefenseActionHistory = true;
